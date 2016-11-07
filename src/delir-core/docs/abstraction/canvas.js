dbbcf2d42d298e43212089a6b06e43ab752ed9db
Ragg <Ragg-@users.noreply.github.com>
2016-11-08 01:45:25 +0900

    remove old documents

--- src/delir-core/docs/abstraction/canvas.js
+++
@@ -1,9 +0,0 @@
-let canvas;
-
-if (typeof window !== 'undefined') {
-    canvas = function Canvas() { return document.createElement('canvas') }
-} else if (typeof global !== 'undefined') {
-    canvas = require('canvas')
-}
-
-export default canvas
