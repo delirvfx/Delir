eb3ab68b10c175e43d0981ea795b3cb641e8f80b
Ragg <Ragg-@users.noreply.github.com>
2017-01-22 19:30:05 +0900

    remove unnecessory code

--- src/delir-core/src/plugin/effect-plugin-base.ts
+++ src/delir-core/src/plugin/effect-plugin-base.ts
@@ -3,19 +3,9 @@ import RenderRequest from '../renderer/render-request'
 import PluginBase from './plugin-base'
 import {TypeDescriptor} from './type-descriptor'
 
-import {
-    InvalidPluginLoadedException
-} from '../exceptions'
-
 export default class EffectPluginBase extends PluginBase
 {
-    static pluginDidLoad()
-    {
-        const subClassName = this ? this.name : '<<do not known>>'
-        throw new InvalidPluginLoadedException(`CustomLayer plugin class \`${subClassName}\` not implement \`static pluginDidLoad\` method`, {
-            class: this,
-        })
-    }
+    static pluginDidLoad() {}
 
     static provideParameters(): TypeDescriptor
     {
