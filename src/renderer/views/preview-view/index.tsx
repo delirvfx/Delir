27f28a48a302741bedd538b397cd230f910bbd53
Ragg <Ragg-@users.noreply.github.com>
2017-03-05 03:22:20 +0900

    Transform PreviewView to tsx

--- src/renderer/views/preview-view/index.tsx
+++ src/renderer/views/preview-view/index.tsx
@@ -1,28 +1,34 @@
-import React, {PropTypes} from 'react'
+import * as React from 'react'
+import {PropTypes} from 'react'
+import * as Delir from 'delir-core'
 
-import EditorStateStore from '../stores/editor-state-store'
-import ProjectModifyStore from '../stores/project-modify-store'
+import connectToStores from '../../utils/connectToStores'
 
-import Pane from './components/pane'
+import EditorStateStore from '../../stores/editor-state-store'
+import ProjectModifyStore from '../../stores/project-modify-store'
 
-export default class PreviewView extends React.Component
+import Pane from '../components/pane'
+
+interface PreviewViewProps {
+    activeComp: Delir.Project.Composition
+}
+
+interface PreviewViewState {
+    scale: number
+}
+
+@connectToStores([EditorStateStore], () => ({
+    activeComp: EditorStateStore.getState().get('activeComp')
+}))
+export default class PreviewView extends React.Component<PreviewViewProps, PreviewViewState>
 {
     constructor()
     {
         super()
 
         this.state = {
-            project: EditorStateStore.getState(),
             scale: 1,
         }
-
-        EditorStateStore.addListener(() => {
-            this.setState({project: EditorStateStore.getState()})
-        })
-
-        ProjectModifyStore.addListener(() => {
-            this.forceUpdate()
-        })
     }
 
     componentDidMount()
@@ -51,20 +57,18 @@ export default class PreviewView extends React.Component
 
     render()
     {
-        const {project} = this.state
-        // console.log(project.activeComp);
-
+        const {activeComp} = this.props
         return (
             <Pane className='view-preview' allowFocus>
                 <div className='inner'>
-                    <div className='header'>{project.activeComp && project.activeComp.name}</div>
+                    <div className='header'>{activeComp && activeComp.name}</div>
                     <div className='view' onWheel={this.onWheel}>
                         <canvas ref='canvas' className='canvas' width='640' height='360' style={{transform:`scale(${this.state.scale})`}}/>
                         <video ref='video' src='../../navcodec.mp4' style={{display:'none'}} controls loop />
                     </div>
                     <div className='footer'>
                         <label>Scale</label>
-                        <select>
+                        <select onChange={this.selectScale}>
                             <option value="50">50%</option>
                             <option value="100">100%</option>
                             <option value="120">120%</option>
