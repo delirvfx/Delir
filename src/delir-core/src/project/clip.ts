a763e7703865875dc47c7132695853555ddb1af1
Ragg <Ragg-@users.noreply.github.com>
2017-03-04 14:00:46 +0900

    Move `Clip` definition from `layer.ts`

---
+++ src/delir-core/src/project/clip.ts
@@ -0,0 +1,97 @@
+import * as _ from 'lodash'
+import Keyframe from './keyframe'
+import Effect from './effect'
+import {LayerScheme, LayerConfigScheme} from './scheme/layer'
+import {RendererProperties} from './types'
+
+export default class Clip
+{
+    static deserialize(layerJson: LayerScheme)
+    {
+        const layer = new Clip
+        const config = _.pick(layerJson.config, [
+            'renderer',
+            'rendererOptions',
+            'placedFrame',
+            'durationFrames',
+            'keyframeInterpolationMethod',
+        ]) as LayerConfigScheme
+
+        const keyframes = _.mapValues(layerJson.keyframes, keyframeSet => {
+            return new Set(Array.from(keyframeSet).map(keyframe => Keyframe.deserialize(keyframe)))
+        })
+
+        Object.defineProperty(layer, 'id', {value: layerJson.id})
+        Object.assign(layer.config, config)
+        layer.keyframes = keyframes
+        return layer
+    }
+
+    id: string|null = null
+
+    config: {
+        renderer: string|null,
+        rendererOptions: RendererProperties,
+        placedFrame: number|null,
+        durationFrames: number|null,
+        keyframeInterpolationMethod: string,
+    } = {
+        renderer: null,
+        rendererOptions: {},
+        placedFrame: null,
+        durationFrames: null,
+        keyframeInterpolationMethod: 'linear',
+    }
+
+    keyframes: {[keyName:string]: Set<Keyframe>} = {}
+    effects: Effect[] = []
+
+    // get id(): string { return this._id }
+
+    get renderer(): string { return this.config.renderer as string }
+    set renderer(renderer: string) { this.config.renderer = renderer }
+
+    get rendererOptions(): RendererProperties { return this.config.rendererOptions }
+    set rendererOptions(rendererOptions: RendererProperties) { this.config.rendererOptions = rendererOptions }
+
+    get placedFrame(): number { return this.config.placedFrame as number }
+    set placedFrame(placedFrame: number) { this.config.placedFrame = placedFrame }
+
+    get durationFrame(): number { throw new Error('layer.durationFrame is discontinuance.') }
+    set durationFrame(durationFrames: number) { throw new Error('layer.durationFrame is discontinuance.') }
+
+    get durationFrames(): number { return this.config.durationFrames as number }
+    set durationFrames(durationFrames: number) { this.config.durationFrames = durationFrames }
+
+    get keyframeInterpolationMethod(): string { return this.config.keyframeInterpolationMethod as string }
+    set keyframeInterpolationMethod(keyframeInterpolationMethod: string) { this.config.keyframeInterpolationMethod = keyframeInterpolationMethod }
+
+    constructor()
+    {
+        Object.seal(this)
+    }
+
+    toPreBSON(): Object
+    {
+        return {
+            id: this.id,
+            config: Object.assign({}, this.config),
+            effects: this.effects.slice(0),
+            keyframes: _.mapValues(this.keyframes, (keyframe, propName) => {
+                return Array.from(keyframe).map(keyframe => keyframe.toPreBSON())
+            }),
+        }
+    }
+
+    toJSON(): Object
+    {
+        return {
+            id: this.id,
+            config: Object.assign({}, this.config),
+            effects: this.effects.slice(0),
+            keyframes: _.mapValues(this.keyframes, (keyframe, propName) => {
+                return Array.from(keyframe).map(keyframe => keyframe.toJSON())
+            }),
+        }
+    }
+}
