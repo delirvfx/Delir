551ce544366987c5e90d4d4f77d24b57d25bc7fa
Ragg <Ragg-@users.noreply.github.com>
2017-04-24 22:34:16 +0900

    Sync horizontal scroll position and zoom for TimelineView / Gradiation View

--- src/renderer/views/TimelaneView/_Gradations.tsx
+++ src/renderer/views/TimelaneView/_Gradations.tsx
@@ -12,6 +12,7 @@ import * as s from './Gradations.styl'
 interface GradationsProps {
     activeComposition: Delir.Project.Composition|null,
     cursorHeight: number,
+    scrollLeft: number,
     scale: number
     pxPerSec: number,
     onSeeked: (frame: number) => any
@@ -27,15 +28,21 @@ export default class Gradations extends Component<GradationsProps, GradationsSta
     protected static propTypes = {
         activeComposition: PropTypes.object.isRequired,
         cursorHeight: PropTypes.number.isRequired,
+        scrollLeft: PropTypes.number,
         scale: PropTypes.number.isRequired,
         pxPerSec: PropTypes.number.isRequired,
         onSeeked: PropTypes.func.isRequired
+    }
+
+    protected static defaultProps = {
+        scrollLeft: 0,
     }
 
     private intervalId: number = -1
 
     private refs: {
         cursor: HTMLDivElement
+        measureLayer: HTMLDivElement
     }
 
     private state = {
@@ -56,20 +63,25 @@ export default class Gradations extends Component<GradationsProps, GradationsSta
     private _updateCursor = () =>
     {
         const renderer = RendererService.renderer
-        const {activeComposition, scale} = this.props
-        const {cursor} = this.refs
+        const {activeComposition, scrollLeft, scale} = this.props
+        const {cursor, measureLayer} = this.refs
 
         if (!renderer) return
 
         if (activeComposition) {
             // Reactの仕組みを使うとrenderMeasureが走りまくってCPUがヤバいので
-            // Reactの輪廻 - Life cycle - から外した
-            cursor.style.left = TimelineHelper.framesToPixel({
+            // Reactのライフサイクルから外す
+            const cursorLeft = TimelineHelper.framesToPixel({
                 pxPerSec: 30,
                 framerate: activeComposition.framerate,
                 durationFrames: renderer.session.lastRenderedFrame,
                 scale,
-            }) + 'px'
+            })
+
+            cursor.style.display = cursorLeft - scrollLeft < 0 ? 'none' : 'block'
+            cursor.style.left = `${cursorLeft}px`
+            cursor.style.transform = `translateX(-${scrollLeft}px)`
+            measureLayer.style.transform = `translateX(-${scrollLeft}px)`
         }
 
         this.intervalId = requestAnimationFrame(this._updateCursor)
@@ -86,7 +98,7 @@ export default class Gradations extends Component<GradationsProps, GradationsSta
 
         if (!this.state.dragSeekEnabled) return
 
-        const {activeComposition, pxPerSec, scale} = this.props
+        const {activeComposition, pxPerSec, scale, scrollLeft} = this.props
 
         if (! activeComposition) return
 
@@ -94,7 +106,7 @@ export default class Gradations extends Component<GradationsProps, GradationsSta
             pxPerSec,
             framerate: activeComposition.framerate,
             scale,
-            pixel: (e as MouseEvent).layerX,
+            pixel: (e as MouseEvent).layerX + scrollLeft,
         }) | 0
 
         this.props.onSeeked(frame)
@@ -110,8 +122,10 @@ export default class Gradations extends Component<GradationsProps, GradationsSta
                 onMouseUp={this._seeking}
                 onClick={this._seeking}
             >
-                <div className={s.measureLayer}>
-                    {this._renderMeasure()}
+                <div className={s.measureLayerTrimer}>
+                    <div ref='measureLayer' className={s.measureLayer}>
+                        {this._renderMeasure()}
+                    </div>
                 </div>
                 <div ref='cursor' className={s.playingCursor} style={{height: `calc(100% + ${this.props.cursorHeight}px - 5px)`}} />
             </div>
@@ -120,17 +134,16 @@ export default class Gradations extends Component<GradationsProps, GradationsSta
 
     private _renderMeasure(): JSX.Element[]
     {
-        const {activeComposition} = this.props
-        if (! activeComposition) {
-            return []
-        }
+        const {activeComposition, scrollLeft} = this.props
+        if (! activeComposition) return []
 
         let previousPos = -40
         const components: JSX.Element[] = []
         for (let idx = 0; idx < 300; idx++) {
-            let frame = 10 * idx
+            const frame = 10 * idx
 
             if (frame >= activeComposition.durationFrames) {
+                // Hit last frame marker
                 const pos = TimelineHelper.framesToPixel({
                     pxPerSec: this.props.pxPerSec,
                     framerate: this.props.activeComposition!.framerate,
