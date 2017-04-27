1ead8ad79e064b34923000cb58abb5511292b6d2
Ragg <Ragg-@users.noreply.github.com>
2017-04-28 01:15:45 +0900

    Add experimental plugins

---
+++ src/experimental-plugins/color-keying/index.ts
@@ -0,0 +1,22 @@
+import {
+    // Type,
+    // TypeDescriptor,
+    LayerPluginBase,
+    // PluginPreRenderRequest,
+    RenderRequest,
+    // Exceptions
+} from 'delir-core'
+
+export default class CompositionLayer extends LayerPluginBase
+{
+    static pluginDidLoad()
+    {
+
+    }
+
+
+    async render(req: RenderRequest)
+    {
+
+    }
+}
