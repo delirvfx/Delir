e37bb05862fb47e15dddc408f5604300ab2788ef
Ragg <Ragg-@users.noreply.github.com>
2017-04-27 22:48:12 +0900

    Sync keyframe editor scrolling with timeline scrolling

--- src/renderer/views/KeyframeView/index.tsx
+++ src/renderer/views/KeyframeView/index.tsx
@@ -423,7 +423,7 @@ export default class KeyframeView extends React.Component<KeyframeViewProps, Key
         const clipPlacedPositionX = this._frameToPx(activeClip.placedFrame) - scrollLeft
 
         return keyframes.slice(0).sort((a, b) => a.frameOnClip - b.frameOnClip).map((kf, idx) => {
-            const x = clipPlacedPositionX + this._frameToPx(kf.frameOnClip)
+            const x = clipPlacedPositionX + this._frameToPx(kf.frameOnClip) - scrollLeft
             const nextX = keyframes[idx + 1] ? clipPlacedPositionX + this._frameToPx(keyframes[idx + 1].frameOnClip) : null
             const transform = (this.state.keyframeMovement && kf.id === this._selectedKeyframeId) ? this.state.keyframeMovement : {x: 0}
 
@@ -471,7 +471,7 @@ export default class KeyframeView extends React.Component<KeyframeViewProps, Key
         const clipPlacedPositionX = this._frameToPx(activeClip.placedFrame)
 
         return keyframes.slice(0).sort((a, b) => a.frameOnClip - b.frameOnClip).map((kf, idx) => {
-            const x = clipPlacedPositionX + this._frameToPx(kf.frameOnClip)
+            const x = clipPlacedPositionX + this._frameToPx(kf.frameOnClip) - scrollLeft
             const nextX = keyframes[idx + 1] ? clipPlacedPositionX + this._frameToPx(keyframes[idx + 1].frameOnClip) : null
             const transform = (this.state.keyframeMovement && kf.id === this._selectedKeyframeId) ? this.state.keyframeMovement : {x: 0}
 
