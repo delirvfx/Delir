8989c97ac125ff7e5ff1a5d9fe29fdeef4efd115
Ragg <Ragg-@users.noreply.github.com>
2017-07-15 17:41:22 +0900

    Set accessor for utils/Record

--- src/frontend/utils/Record.ts
+++ src/frontend/utils/Record.ts
@@ -7,15 +7,17 @@ export default class Record<R> {
         RecordClass.call(this)
     }
 
-    get<K extends keyof R>(key: K): R[K] {
+    public get<K extends keyof R>(key: K): R[K]
+    {
         return ImmutableRecord.prototype.get.call(key)
     }
 
-    set<K extends keyof R>(key: K, value: R[K]): Record<R> {
+    public set<K extends keyof R>(key: K, value: R[K]): Record<R>
+    {
         return ImmutableRecord.prototype.set.call(this, key, value)
     }
 
-    equals(other: Record<any>): boolean
+    public equals(other: Record<any>): boolean
     {
         return ImmutableRecord.prototype.equals(other)
     }
