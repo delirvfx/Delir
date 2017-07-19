70a9cbba51336bc76a54df38f3555c0383451caf
Ragg <Ragg-@users.noreply.github.com>
2017-07-19 21:46:54 +0900

    Remove old Renderer codes

--- src/delir-core/src/renderer/index.ts
+++ src/delir-core/src/renderer/index.ts
@@ -2,5 +2,4 @@ import * as Renderer from './renderer/index'
 
 export {expressionContextTypeDefinition} from './pipeline/ExpressionContext'
 export {default as Pipeline} from './pipeline/pipeline'
-export {default as Renderer} from './pipeline/renderer'
 export {Renderer as Renderers}
