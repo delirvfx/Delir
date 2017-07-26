66dbd23c07f9e455cce1fc2d184ad667b54310c9
Ragg <Ragg-@users.noreply.github.com>
2017-07-27 01:58:41 +0900

    Force English mode on development environment

--- src/frontend/utils/I18n.ts
+++ src/frontend/utils/I18n.ts
@@ -21,7 +21,7 @@ const replacePlaceHolder = (text: string, params: {[name: string]: any}): string
 }
 
 export default (json: I18nTexts) => {
-    const lang = __DEV__ ? 'ja' : navigator.language
+    const lang = __DEV__ ? 'en' : navigator.language
 
     return (key: string|string[], params?: {[name: string]: any}): string => {
         const text = (_.get(json[lang], key) || _.get(json.en, key)) as string|null
