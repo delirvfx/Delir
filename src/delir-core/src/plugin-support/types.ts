bbbeacdc84cb598054fab0baf841286a14ec9c74
Ragg <Ragg-@users.noreply.github.com>
2017-07-28 02:18:40 +0900

    type -> interface and trim trailing comma

--- src/delir-core/src/plugin-support/types.ts
+++ src/delir-core/src/plugin-support/types.ts
@@ -5,45 +5,44 @@ export type PluginFeatures = 'Effect' | 'CustomLayer' | 'ExpressionExtension'
 export interface PackageJSONDelirSection {
     feature: PluginFeatures,
     /** Accept file types by mimeType:propName */
-    acceptFileTypes: {[mimeType: string]: string},
+    acceptFileTypes: {[mimeType: string]: string}
 }
 
 export interface DelirPluginPackageJson {
-    name: string,
-    version: string,
-    author: string|Array<string>,
-    main?: string,
+    name: string
+    version: string
+    author: string|string[]
+    main?: string
     engines: {
-        delir: string,
+        delir: string
     },
-    delir: PackageJSONDelirSection,
+    delir: PackageJSONDelirSection
 }
 
-
 // type of before load PluginEntry
-export type PluginEntryFragment = {
-    id: string,
-    package: DelirPluginPackageJson,
-    pluginInfo: PackageJSONDelirSection,
-    packageRoot: string,
-    entryPath: string,
-    class?: typeof PluginBase,
+export interface PluginEntryFragment {
+    id: string
+    package: DelirPluginPackageJson
+    pluginInfo: PackageJSONDelirSection
+    packageRoot: string
+    entryPath: string
+    class?: typeof PluginBase
     // parameters?: TypeDescriptor
 }
 
-export type PluginEntry = {
-    id: string,
-    package: DelirPluginPackageJson,
-    pluginInfo: PackageJSONDelirSection,
-    packageRoot: string,
-    entryPath: string,
-    class: typeof PluginBase,
+export interface PluginEntry {
+    id: string
+    package: DelirPluginPackageJson
+    pluginInfo: PackageJSONDelirSection
+    packageRoot: string
+    entryPath: string
+    class: typeof PluginBase
     // parameters: TypeDescriptor
 }
 
-export type PluginSummary = {
-    id: string,
-    type: PluginFeatures,
-    path: string,
-    package: DelirPluginPackageJson,
+export interface PluginSummary {
+    id: string
+    type: PluginFeatures
+    path: string
+    package: DelirPluginPackageJson
 }
