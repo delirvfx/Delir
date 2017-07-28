6b13d10f41b893dc7eb216d529ef3b021bb3c791
Ragg <Ragg-@users.noreply.github.com>
2017-07-29 04:16:06 +0900

    Layer name now can renamed via context menu

--- src/frontend/views/TimelineView/LaneLabel.i18n.ts
+++ src/frontend/views/TimelineView/LaneLabel.i18n.ts
@@ -5,12 +5,14 @@ export default I18n({
         contextMenu: {
             createLayer: 'レイヤーを追加',
             removeLayer: 'レイヤーを削除',
+            renameLayer: 'レイヤー名を変更',
         },
     },
     en: {
         contextMenu: {
             createLayer: 'Add layer',
             removeLayer: 'Remove layer',
+            renameLayer: 'Rename layer',
         },
     }
 })
