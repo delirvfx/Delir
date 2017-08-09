457d3b689e7470508b4e526d2b7b2a433e6d76f9
Ragg <Ragg-@users.noreply.github.com>
2017-08-10 02:19:36 +0900

    Add WIP specs

---
+++ src/frontend/__spec__/fixtures/Project.ts
@@ -0,0 +1,155 @@
+import * as Delir from 'delir-core'
+import {ProjectHelper, Values} from 'delir-core'
+import {join} from 'path'
+
+const assign = <T>(dest: T, ...sources: Partial<T>[]): T => Object.assign(dest as any, ...sources)
+
+const fps = 30
+const durationFrames = fps * 10
+const project = new Delir.Project.Project()
+
+const movieAsset = assign(new Delir.Project.Asset(), {
+    name: 'Movie',
+    fileType: 'mp4',
+    path: join(process.cwd(), 'video.mp4'),
+})
+
+const audioAsset = assign(new Delir.Project.Asset(), {
+    name: 'Audio',
+    fileType: 'mp3',
+    path: join(process.cwd(), 'audio.mp3'),
+})
+
+const audioAsset2 = assign(new Delir.Project.Asset(), {
+    name: 'Audio',
+    fileType: 'mp3',
+    path: join(process.cwd(), 'audio.mp3'),
+})
+
+const imageAsset = assign(new Delir.Project.Asset(), {
+    name: 'Image',
+    fileType: 'jpg',
+    path: join(process.cwd(), 'image.jpg'),
+})
+
+;[movieAsset, audioAsset, audioAsset2, imageAsset].forEach(a => ProjectHelper.addAsset(project, a))
+
+// Maser Composition
+const c1 = assign(new Delir.Project.Composition(), {
+    name: 'Master Composition',
+    width: 640,
+    height: 360,
+    framerate: fps,
+    durationFrames,
+    audioChannels: 2,
+    samplingRate: 48000,
+    backgroundColor: new Values.ColorRGB(0, 188, 255),
+})
+
+const layer1 = assign(new Delir.Project.Layer(), {
+    name: 'Audio',
+})
+
+const layer2 = assign(new Delir.Project.Layer(), {
+    name: '🔥 FIRE 🔥',
+})
+
+const layer3 = assign(new Delir.Project.Layer(), {
+    name: 'NYAN = ^ . ^ = CAT',
+})
+
+const layer4 = assign(new Delir.Project.Layer(), {
+    name: 'video',
+})
+
+const movieClip = assign(new Delir.Project.Clip(), {
+    renderer: 'video',
+    placedFrame: 0,
+    durationFrames,
+    keyframes: {
+        source: [
+            assign(new Delir.Project.Keyframe(), {
+                value: {assetId: movieAsset.id},
+                frameOnClip: 0,
+            })
+        ]
+    }
+})
+
+const textClip = assign(new Delir.Project.Clip(), {
+    renderer: 'text',
+    placedFrame: 0,
+    durationFrames,
+    keyframes: {
+        text: [
+            assign(new Delir.Project.Keyframe(), {value: 'test', frameOnClip: 0})
+        ],
+        source: [
+            assign(new Delir.Project.Keyframe(), {value: {assetId: movieAsset.id}, frameOnClip: 0})
+        ],
+        loop: [
+            assign(new Delir.Project.Keyframe(), {value: true, frameOnClip: 0}),
+        ],
+        x: [
+            assign(new Delir.Project.Keyframe(), {value: 0, frameOnClip: 0, easeOutParam: [.4, .5]}),
+            assign(new Delir.Project.Keyframe(), {value: 300, frameOnClip: 600, easeInParam: [.6, .5]}),
+        ],
+    },
+    expressions: {
+        text: new Values.Expression('typescript', 'console.log(duration);\n`time:${time}\\nframe:${frame}`')
+    },
+})
+
+const audioClip = assign(new Delir.Project.Clip(), {
+    // renderer: 'video'
+    renderer: 'audio',
+    placedFrame: 0,
+    durationFrames,
+    keyframes: {
+        source: [
+            assign(new Delir.Project.Keyframe(), {
+                value: {assetId: audioAsset.id},
+                frameOnClip: 0
+            }),
+        ]
+    }
+})
+
+const imageClip = assign(new Delir.Project.Clip(), {
+    renderer: 'image',
+    placedFrame: 20,
+    durationFrames,
+    keyframes: {
+        source: [
+            assign(new Delir.Project.Keyframe(), {
+                value: {assetId: imageAsset.id},
+                frameOnClip: 0,
+            })
+        ],
+        x: [
+            assign(new Delir.Project.Keyframe(), {
+                value: 0,
+                frameOnClip: -10,
+            }),
+            assign(new Delir.Project.Keyframe(), {
+                value: 10,
+                frameOnClip: 20,
+            }),
+            assign(new Delir.Project.Keyframe(), {
+                value: 40,
+                frameOnClip: 100,
+            }),
+        ]
+    }
+})
+
+ProjectHelper.addComposition(project, c1)
+;[layer1, layer2, layer3, layer4].forEach(lane => ProjectHelper.addLayer(project, c1, lane))
+
+ProjectHelper.addClip(project, layer1, imageClip)
+ProjectHelper.addClip(project, layer2, audioClip)
+ProjectHelper.addClip(project, layer3, movieClip)
+ProjectHelper.addClip(project, layer4, textClip)
+
+export const IMAGE_LAYER_INDEX = 0
+export default project
---
+++ src/frontend/views/KeyframeEditor/__spec__/KeyframeGraph-spec.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import * as ReactTestRenderer from 'react-test-renderer'
+import * as jsdom from 'jsdom-global'
+import * as Delir from 'delir-core'
+
+import {default as project, IMAGE_LAYER_INDEX} from '../../../__spec__/fixtures/Project'
+import KeyframeGraph from '../KeyframeGraph.tsx'
+
+describe('KeyframeEditor.KeyframeGraph', () => {
+    before(function() { (this as any).jsdom = jsdom() })
+    after(function() { (this as any).jsdom() })
+
+    // it('test', () => {
+    //     ReactTestRenderer.create(
+    //         <KeyframeGraph
+    //             width={1000}
+    //             height={300}
+    //             scrollLeft={0}
+    //             viewBox='0 0 1000 300'
+    //             composition={project.compositions[0]}
+    //             clip={project.compositions[0].layers[0].clips[0]}
+    //             propName='x'
+    //             keyframes={project.compositions[0].layers[IMAGE_LAYER_INDEX].clips[0].keyframes.x}
+    //             pxPerSec={30}
+    //             zoomScale={1}
+    //         />
+    //     )
+    // })
+})
