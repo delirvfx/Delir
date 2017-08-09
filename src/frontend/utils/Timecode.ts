a90930e5d537e8067a056869b82e78efc63478ec
Ragg <Ragg-@users.noreply.github.com>
2017-08-10 01:22:55 +0900

    Add Timecode utility

---
+++ src/frontend/utils/Timecode.ts
@@ -0,0 +1,11 @@
+
+
+export const frameToTimeCode = (frame: number, frameRate: number) => {
+    const seconds = frame / frameRate
+    const timeFrame = (frame % frameRate).toString().padStart(2, '0')
+    const timeSeconds = (seconds % 60 | 0).toString().padStart(2, '0')
+    const timeMinutes = ((seconds / 60) % 60 | 0).toString().padStart(2, '0')
+    const timeHour = ((seconds / 60 / 60) | 0).toString().padStart(2, '0')
+
+    return `${timeHour}:${timeMinutes}:${timeSeconds}:${timeFrame}`
+}
