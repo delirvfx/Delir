2dc3fce009ad044ebf3145eaaaffa8a08874e6de
Ragg <Ragg-@users.noreply.github.com>
2017-08-25 00:00:44 +0900

    Improve typing

--- src/delir-core/src/project/effect.ts
+++ src/delir-core/src/project/effect.ts
@@ -57,7 +57,7 @@ export default class Effect
         Object.seal(this)
     }
 
-    public toPreBSON(): Object
+    public toPreBSON(): EffectOptionScheme
     {
         return {
             id: this.id,
@@ -69,7 +69,7 @@ export default class Effect
         }
     }
 
-    public toJSON(): Object
+    public toJSON(): EffectOptionScheme
     {
         return {
             id: this.id,
