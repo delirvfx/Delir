3efd811d9b56f127d0ffcdcde7991a8be8565214
Ragg <Ragg-@users.noreply.github.com>
2017-08-29 02:48:54 +0900

    Deprecation Color{RGB,RGBA}#toString (add #toCSSColor instead)

--- src/delir-core/src/values/color-rgb.ts
+++ src/delir-core/src/values/color-rgb.ts
@@ -49,6 +49,14 @@ export default class ColorRGB {
         }
     }
 
+    public toCSSColor()
+    {
+        return `rgba(${this.r|0}, ${this.g|0}, ${this.b|0}, ${this.a})`
+    }
+
+    /**
+     * @deprecated
+     */
     public toString()
     {
         return `rgba(${this.r|0}, ${this.g|0}, ${this.b|0}, 1)`
--- src/delir-core/src/values/color-rgba.ts
+++ src/delir-core/src/values/color-rgba.ts
@@ -58,6 +58,14 @@ export default class ColorRGBA {
         }
     }
 
+    public toCSSColor()
+    {
+        return `rgba(${this.r|0}, ${this.g|0}, ${this.b|0}, ${this.a})`
+    }
+
+    /**
+     * @deprecated
+     */
     public toString()
     {
         return `rgba(${this.r|0}, ${this.g|0}, ${this.b|0}, ${this.a})`
