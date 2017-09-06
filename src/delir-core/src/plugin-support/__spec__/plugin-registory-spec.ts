c4fda0b0dc8b27ee70786b3d1c2ac95815139f26
Ragg <Ragg-@users.noreply.github.com>
2017-09-06 22:35:36 +0900

    Fix test

--- src/delir-core/src/plugin-support/__spec__/plugin-registory-spec.ts
+++ src/delir-core/src/plugin-support/__spec__/plugin-registory-spec.ts
@@ -1,17 +1,3 @@
-// @flow
-<<<<<<< HEAD
-import path from 'path'
-import FSPluginLoader from '../FSPluginLoader'
-
-describe('PluginRegistry', () => {
-    it('exporting: PluginFeatures', () => {
-
-    it('loading plugins', async () => {
-        // mock missing method in mocha
-
-        const r = new FSPluginLoader()
-        const result = await r.loadPackageDir(path.join(__dirname, '../../src/plugins'))
-=======
 import * as path from 'path'
 import FSPluginLoader from '../FSPluginLoader'
 
@@ -19,7 +5,6 @@ describe('PluginRegistry', () => {
     it('loading plugins', async () => {
         const r = new FSPluginLoader()
         const result = await r.loadPackageDir(path.join(__dirname, '../../../fixtures/plugins'))
->>>>>>> 7a26a5a... Fix test
 
         expect(result).to.have.key('loaded')
         expect(result).to.have.key('failed')
