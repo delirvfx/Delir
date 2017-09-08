f5f03d203cde06a757fe7cb3747db52c59ed2cdb
Ragg <Ragg-@users.noreply.github.com>
2017-09-09 02:40:10 +0900

    Add AppAction translation

---
+++ src/frontend/actions/App.i18n.ts
@@ -0,0 +1,15 @@
+import I18n from '../utils/I18n'
+import { isMacOS } from '../utils/platform'
+
+export default I18n({
+    ja: {
+        saved: '保存しました！',
+        letsSave: `そろそろプロジェクトを保存しましょう (${isMacOS() ? 'Command' : 'Ctrl'}+S)`,
+        autoSaved: 'プロジェクトを自動保存しました (:fileName)',
+    },
+    en: {
+        saved: 'Project saved.',
+        letsSave: `Let\'s save the project soon. (${isMacOS() ? 'Command' : 'Ctrl'}+S)`,
+        autoSaved: 'Project auto saved. (:fileName)',
+    }
+})
