66b44b8c2b3d802b5d9a6bec6a9b4e48da25c0f0
Ragg <Ragg-@users.noreply.github.com>
2018-02-11 01:27:09 +0900

    WIP

---
+++ packages/delir-core/src/Engine/Work/CompositionRenderWork.spec.ts
@@ -0,0 +1,5 @@
+import {} from 'jest'
+
+describe('CompositionRenderWork', () => {
+    it.skip('TODO')
+})
--- packages/delir-core/src/Engine/Work/Work.ts
+++
@@ -1,5 +0,0 @@
-import FrameContext from 'Engine/FrameContext'
-
-export interface Work {
-    work(ctx: FrameContext): Promise<void>
-}
