fa6b4d5581cfbf14273b0d8fbf4097b44923d081
Ragg <Ragg-@users.noreply.github.com>
2018-04-14 18:42:27 +0900

    Add benchmark

--- packages/fleur-benchmarks/src/fleur.spec.ts
+++ packages/fleur-benchmarks/src/fleur.spec.ts
@@ -4,12 +4,18 @@ import * as ReactDOM from 'react-dom'
 import { connectToStores, ContextProp, createElementWithContext, withComponentContext } from '../../fleur-react/src'
 
 describe('benchmark', () => {
-    it('Fleur', async () => {
+    it('Fleur / rendering time', async () => {
+        const numOfDispatches = 10000
+        const callCounter = jest.fn()
+
         const incrementAction = action()
         const incrementOperation = operation((ctx) => { ctx.dispatch(incrementAction, {}) })
         class TestStore extends Store<{count: number}> {
             public state = { count: 0 }
-            private handleIncrement = listen(incrementAction, (payload) => { this.produce(d => d.count++ ) })
+            private handleIncrement = listen(incrementAction, (payload) => {
+                callCounter()
+                this.produce(d => d.count++ )
+            })
             get count() { return this.state.count }
         }
 
@@ -28,12 +34,53 @@ describe('benchmark', () => {
         context.getStore(TestStore)
         const div = document.createElement('div')
 
-        console.time('Fleur')
-        for (let count = 1; count < 10000; count++) {
+        console.time(`Fleur dispatch action ${numOfDispatches} times`)
+        for (let count = 1; count < numOfDispatches + 1; count++) {
             await context.executeOperation(incrementOperation, {})
             await new Promise(r => ReactDOM.render(createElementWithContext(context, Component, {}), div, r))
             expect(div.innerHTML).toBe(`<div>${count}</div>`)
         }
-        console.timeEnd('Fleur')
+        expect(callCounter.mock.calls.length).toBe(numOfDispatches)
+        console.timeEnd(`Fleur dispatch action ${numOfDispatches} times`)
+    })
+
+    it('Fleur / Store update time', async () => {
+        const numOfStores = 100000
+        const callCounter = jest.fn()
+
+        const incrementAction = action()
+        const incrementOperation = operation((ctx) => { ctx.dispatch(incrementAction, {}) })
+        const stores = Array.from(Array(numOfStores)).map(_ => (
+            class TestStore extends Store<{count: number}> {
+                public state = { count: 0 }
+                private handleIncrement = listen(incrementAction, (payload) => {
+                    callCounter()
+                    this.produce(d => d.count++ )
+                })
+                get count() { return this.state.count }
+            }
+        ))
+
+        const Component = withComponentContext(connectToStores(stores, (ctx) => ({
+            values: stores.map(s => { ctx.getStore(s).count })
+        }))(
+            class extends React.Component<{count: number} & ContextProp> {
+                public render() {
+                    return null
+                }
+            }
+        ))
+
+        const app = new Fleur({ stores })
+
+        const context = app.createContext()
+        stores.forEach(s => context.getStore(s))
+        const div = document.createElement('div')
+
+        console.time(`Fleur update ${numOfStores} stores once`)
+        await context.executeOperation(incrementOperation, {})
+        await new Promise(r => ReactDOM.render(createElementWithContext(context, Component, {}), div, r))
+        expect(callCounter.mock.calls.length).toBe(numOfStores)
+        console.timeEnd(`Fleur update ${numOfStores} stores once`)
     })
 })
--- packages/fleur-benchmarks/src/fluxible.spec.ts
+++ packages/fleur-benchmarks/src/fluxible.spec.ts
@@ -8,6 +8,9 @@ import * as ReactDOM from 'react-dom'
 
 describe('benchmark', () => {
     it('Fluxible', async () => {
+        const numOfDispatches = 10000
+        const callCounter = jest.fn()
+
         const incrementOperation = (ctx) => { ctx.dispatch('INCREMENT', {}) }
 
         class TestStore extends (BaseStore as any) {
@@ -18,6 +21,7 @@ describe('benchmark', () => {
 
             public state = { count: 0 }
             private handleIncrement(payload) {
+                callCounter()
                 this.state = { count: this.state.count + 1 }
             }
 
@@ -41,12 +45,61 @@ describe('benchmark', () => {
         context.getStore(TestStore)
         const div = document.createElement('div')
 
-        console.time('Fluxible')
-        for (let count = 1; count < 10000; count++) {
+        console.time(`Fluxible dispatch action ${numOfDispatches} times`)
+        for (let count = 1; count < numOfDispatches + 1; count++) {
             await context.executeAction(incrementOperation, {})
             await new Promise(r => ReactDOM.render(createElementWithContext(context, {}), div, r))
             expect(div.innerHTML).toBe(`<div>${count}</div>`)
         }
-        console.timeEnd('Fluxible')
+        expect(callCounter.mock.calls.length).toBe(numOfDispatches)
+        console.timeEnd(`Fluxible dispatch action ${numOfDispatches} times`)
+    })
+
+    it('Fluxible / Store update time', async () => {
+        const numOfStores = 100000
+        const callCounter = jest.fn()
+
+        const incrementOperation = (ctx) => { ctx.dispatch('INCREMENT', {}) }
+        const stores = Array.from(Array(numOfStores)).map((_, idx) => (
+            class TestStore extends (BaseStore as any) {
+                public static storeName = `TestStore${idx}`
+                public static handlers = {
+                    INCREMENT: 'handleIncrement'
+                }
+
+                public state = { count: 0 }
+                private handleIncrement(payload) {
+                    callCounter()
+                    this.state = { count: this.state.count + 1 }
+                }
+
+                get count() { return this.state.count }
+            }
+        ))
+
+        const Component = connectToStores(stores, (ctx) => ({
+            values: stores.map(s => ctx.getStore(s).count)
+        }))(
+            class extends React.Component<{count: number}> {
+                public render() {
+                    return null // React.createElement('div', {}, `${this.props.count}`)
+                }
+            }
+        )
+
+        const app = new Fluxible({
+            component: Component,
+            stores: stores,
+        })
+
+        const context = app.createContext()
+        stores.forEach(s => context.getStore(s))
+        const div = document.createElement('div')
+
+        console.time(`Fluxible update ${numOfStores} stores once`)
+        await context.executeAction(incrementOperation, {})
+        await new Promise(r => ReactDOM.render(createElementWithContext(context, {}), div, r))
+        expect(callCounter.mock.calls.length).toBe(numOfStores)
+        console.timeEnd(`Fluxible update ${numOfStores} stores once`)
     })
 })
