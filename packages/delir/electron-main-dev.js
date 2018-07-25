a65c3a716d5f7371cdd81ad2d04aa84cfdd16db8
ragg <ra-gg@users.noreply.github.com>
2018-07-25 14:54:13 +0900

    💄

--- packages/delir/electron-main-dev.js
+++ packages/delir/electron-main-dev.js
@@ -5,6 +5,6 @@ const __DEV__ = process.env.NODE_ENV === 'development';
 let mainWindow
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({ width: 1920, height: 1080 });
+    mainWindow = new BrowserWindow({ width: 1920, height: 1080 })
     mainWindow.loadURL(__DEV__ ? 'http://localhost:3000' : `file://${__dirname}/prebuild/index.html`)
 })
