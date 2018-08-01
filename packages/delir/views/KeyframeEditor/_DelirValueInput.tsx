90aa227f7986447227c9613737eb43ab97dd71bd
ragg <ra-gg@users.noreply.github.com>
2018-08-02 02:43:06 +0900

    Refactor to using React.Ref / React.Fragment

--- packages/delir/views/KeyframeEditor/_DelirValueInput.tsx
+++ packages/delir/views/KeyframeEditor/_DelirValueInput.tsx
@@ -23,25 +23,25 @@ export default class DelirValueInput extends React.PureComponent<DelirValueInput
     public state = {
         value: this.props.value,
     }
-    private ref: {
-        numberInput?: DragNumberInput,
-        propX?: DragNumberInput,
-        propY?: DragNumberInput,
-        propZ?: DragNumberInput,
-        propWidth?: DragNumberInput,
-        propHeight?: DragNumberInput,
-        propDepth?: DragNumberInput,
-        colorPicker?: ChromePicker,
-        checkbox?: HTMLInputElement,
-        textArea?: HTMLTextAreaElement,
-        enumSelect?: HTMLSelectElement,
-        assetSelect?: HTMLSelectElement,
+    private ref = {
+        numberInput: React.createRef<DragNumberInput>(),
+        propX: React.createRef<DragNumberInput>(),
+        propY: React.createRef<DragNumberInput>(),
+        propZ: React.createRef<DragNumberInput>(),
+        propWidth: React.createRef<DragNumberInput>(),
+        propHeight: React.createRef<DragNumberInput>(),
+        propDepth: React.createRef<DragNumberInput>(),
+        colorPicker: React.createRef<ChromePicker>(),
+        checkbox: React.createRef<HTMLInputElement>(),
+        textArea: React.createRef<HTMLTextAreaElement>(),
+        enumSelect: React.createRef<HTMLSelectElement>(),
+        assetSelect: React.createRef<HTMLSelectElement>(),
 
-        textSummary?: HTMLInputElement
-        textInputDropdown?: Dropdown
+        textSummary: React.createRef<HTMLInputElement>(),
+        textInputDropdown: React.createRef<Dropdown>(),
 
-        colorPickerDropdown?: Dropdown
-    } = {}
+        colorPickerDropdown: React.createRef<Dropdown>(),
+    }
 
     public componentWillReceiveProps(nextProps: Readonly<DelirValueInputProps>, nextContext: any)
     {
@@ -54,118 +54,127 @@ export default class DelirValueInput extends React.PureComponent<DelirValueInput
     {
         const {descriptor, assets} = this.props
         const {value} = this.state
-        let component: JSX.Element[] = []
-
+        let component: JSX.Element | null
         switch (descriptor.type) {
-        //     case 'POINT_2D':
-        //         component = [
-        //             <DragNumberInput ref='propX' value={value.x} onChange={this.valueChanged} />,
-        //             <span className='separator'>,</span>,
-        //             <DragNumberInput ref='propY' value={value.y} onChange={this.valueChanged} />
-        //         ]
-        //         break
-        //     case 'POINT_3D':
-        //         component = [
-        //             <DragNumberInput ref='propX' value={value.x} onChange={this.valueChanged} />,
-        //             <span className='separator'>,</span>,
-        //             <DragNumberInput ref='propY' value={value.y} onChange={this.valueChanged} />,
-        //             <span className='separator'>,</span>,
-        //             <DragNumberInput ref='propZ' value={value.z} onChange={this.valueChanged} />
-        //         ]
-        //         break
-        //     case 'SIZE_2D':
-        //         component = [
-        //             <DragNumberInput ref='propWidth' value={value.width} onChange={this.valueChanged} />,
-        //             <span className='separator'>x</span>,
-        //             <DragNumberInput ref='propHeight' value={value.height} onChange={this.valueChanged} />
-        //         ]
-        //         break
-        //     case 'SIZE_3D':
-        //         component = [
-        //             <DragNumberInput ref='propWidth' onChange={this.valueChanged} />,
-        //             <span className='separator'>x</span>,
-        //             <DragNumberInput ref='propHeight' onChange={this.valueChanged} />,
-        //             <span className='separator'>x</span>,
-        //             <DragNumberInput ref='propDepth' onChange={this.valueChanged} />
-        //         ]
-        //         break
+            // case 'POINT_2D':
+            //     component = [
+            //         <DragNumberInput ref='propX' value={value.x} onChange={this.valueChanged} />,
+            //         <span className='separator'>,</span>,
+            //         <DragNumberInput ref='propY' value={value.y} onChange={this.valueChanged} />
+            //     ]
+            //     break
+            // case 'POINT_3D':
+            //     component = [
+            //         <DragNumberInput ref='propX' value={value.x} onChange={this.valueChanged} />,
+            //         <span className='separator'>,</span>,
+            //         <DragNumberInput ref='propY' value={value.y} onChange={this.valueChanged} />,
+            //         <span className='separator'>,</span>,
+            //         <DragNumberInput ref='propZ' value={value.z} onChange={this.valueChanged} />
+            //     ]
+            //     break
+            // case 'SIZE_2D':
+            //     component = [
+            //         <DragNumberInput ref='propWidth' value={value.width} onChange={this.valueChanged} />,
+            //         <span className='separator'>x</span>,
+            //         <DragNumberInput ref='propHeight' value={value.height} onChange={this.valueChanged} />
+            //     ]
+            //     break
+            // case 'SIZE_3D':
+            //     component = [
+            //         <DragNumberInput ref='propWidth' onChange={this.valueChanged} />,
+            //         <span className='separator'>x</span>,
+            //         <DragNumberInput ref='propHeight' onChange={this.valueChanged} />,
+            //         <span className='separator'>x</span>,
+            //         <DragNumberInput ref='propDepth' onChange={this.valueChanged} />
+            //     ]
+            //     break
 
             case 'COLOR_RGB':
             case 'COLOR_RGBA':
-                component = [
-                    <button
-                        className={s.colorPickerOpenner}
-                        style={{
-                            backgroundColor: (value as Delir.Values.ColorRGBA).toString()
-                        }}
-                        onClick={this.openColorPicker}
-                    />,
-                    <Dropdown ref={this.bindColorPickerDropdown} className={s.colorPickerContainer}>
-                        <button className={s.colorPickerCloser} onClick={this.closeColorPicker}>Close</button>
-                        <ChromePicker ref={this.bindColorPicker} color={value.toString()} onChange={this.valueChanged} disableAlpha={descriptor.type === 'COLOR_RGB'} />
-                    </Dropdown>
-                ]
+                component = (
+                    <>
+                        <button
+                            className={s.colorPickerOpenner}
+                            style={{
+                                backgroundColor: (value as Delir.Values.ColorRGBA).toString()
+                            }}
+                            onClick={this.openColorPicker}
+                        />
+                        <Dropdown ref={this.ref.colorPickerDropdown} className={s.colorPickerContainer}>
+                            <button className={s.colorPickerCloser} onClick={this.closeColorPicker}>Close</button>
+                            <ChromePicker ref={this.ref.colorPicker} color={value.toString()} onChange={this.valueChanged} disableAlpha={descriptor.type === 'COLOR_RGB'} />
+                        </Dropdown>
+                    </>
+                )
                 break
 
             case 'BOOL':
-                component = [<input ref={this.bindCheckbox} type='checkbox' className={s.checkbox} checked={value as boolean} onChange={this.valueChanged} />]
+                component = <input ref={this.ref.checkbox} type='checkbox' className={s.checkbox} checked={value as boolean} onChange={this.valueChanged} />
                 break
 
             case 'STRING':
-                component = [
-                    <Dropdown ref={this.bindTextInputDropdown}>
-                        <textarea ref={this.bindTextArea} className={s.textArea} onKeyDown={this.onKeydownTextArea} defaultValue={value as string} />
-                    </Dropdown>,
-                    <input ref={this.bindTextSummary} type='text' className={s.textInput} onFocus={this.onFocusTextInput} value={value as string} readOnly />
-                ]
+                component = (
+                    <>
+                        <Dropdown ref={this.ref.textInputDropdown}>
+                            <textarea ref={this.ref.textArea} className={s.textArea} onKeyDown={this.onKeydownTextArea} defaultValue={value as string} />
+                        </Dropdown>
+                        <input ref={this.ref.textSummary} type='text' className={s.textInput} onFocus={this.onFocusTextInput} value={value as string} readOnly />
+                    </>
+                )
                 break
 
             case 'FLOAT':
             case 'NUMBER':
-                component = [<DragNumberInput ref={this.bindNumberInput} value={value as number} onChange={this.valueChanged} allowFloat={descriptor.type === 'FLOAT'} />]
+                component = <DragNumberInput ref={this.ref.numberInput} value={value as number} onChange={this.valueChanged} allowFloat={descriptor.type === 'FLOAT'} />
                 break
 
             case 'FLOAT':
-            case 'CLIP':
-            case 'PULSE':
-                component = []
+            // case 'CLIP':
+            // case 'PULSE':
+                component = <></>
                 break
 
             case 'ENUM':
-                component = [
-                    <select ref={this.bindEnumSelect} value={value ? (value as string) : ''} onChange={this.valueChanged}>
-                        <option></option>
-                        {descriptor.selection.map(item => <option value={item}>{item}</option>)}
-                    </select>
-                ]
+                component = (
+                    <>
+                        <select ref={this.ref.enumSelect} value={value ? (value as string) : ''} onChange={this.valueChanged}>
+                            <option></option>
+                            {descriptor.selection.map(item => <option value={item}>{item}</option>)}
+                        </select>
+                    </>
+                )
                 break
 
             case 'ASSET': {
-                const acceptedAssets = assets.filter(asset => descriptor.extensions.includes(asset.fileType))
+                const acceptedAssets = assets!.filter(asset => descriptor.extensions.includes(asset.fileType))
 
-                component = [
-                    <select ref={this.bindAssetSelect} value={value ? (value as {assetId: string}).assetId! : undefined} onChange={this.valueChanged}>
-                        {acceptedAssets.length === 0 && (
-                            <option selected disabled>{t('asset.empty')}</option>
-                        )}
-                        {acceptedAssets.length > 0 && ([
-                            <option />,
-                            ...acceptedAssets.map(asset => (<option value={asset.id as string}>{asset.name}</option>))
-                        ])}
-                    </select>
-                ]
+                component = (
+                    <>
+                        <select ref={this.ref.assetSelect} value={value ? (value as {assetId: string}).assetId! : undefined} onChange={this.valueChanged}>
+                            {acceptedAssets.length === 0 && (
+                                <option selected disabled>{t('asset.empty')}</option>
+                            )}
+                            {acceptedAssets.length > 0 && (
+                                <>
+                                    <option />
+                                    {...acceptedAssets.map(asset => (<option value={asset.id as string}>{asset.name}</option>))}
+                                </>
+                            )}
+                        </select>
+                    </>
+                )
                 break
             }
 
-            case 'ARRAY':
+            // case 'ARRAY':
             default:
-                component = []
+                component = <></>
         }
 
         // destruct component list to make function `ref` property
         return (
             <div>
-                {...component}
+                {component}
             </div>
         )
     }
@@ -201,21 +210,21 @@ export default class DelirValueInput extends React.PureComponent<DelirValueInput
 
             case 'COLOR_RGB': {
                 const {colorPicker} = this.ref
-                const rgb = colorPicker.state.rgb
+                const rgb = colorPicker.current!.state.rgb
                 this.props.onChange(descriptor, new Delir.Values.ColorRGB(rgb.r, rgb.g, rgb.b))
                 break
             }
 
             case 'COLOR_RGBA': {
                 const {colorPicker} = this.ref
-                const rgba = colorPicker.state.rgb
+                const rgba = colorPicker.current!.state.rgb
                 this.props.onChange(descriptor, new Delir.Values.ColorRGBA(rgba.r, rgba.g, rgba.b, rgba.a))
                 break
             }
 
             case 'ENUM': {
                 const {enumSelect} = this.ref
-                this.props.onChange(descriptor, enumSelect.value)
+                this.props.onChange(descriptor, enumSelect.current!.value)
                 break
             }
 
@@ -228,21 +237,21 @@ export default class DelirValueInput extends React.PureComponent<DelirValueInput
 
             case 'BOOL': {
                 const {checkbox} = this.ref
-                checkbox.checked = !this.props.value
+                checkbox.current!.checked = !this.props.value
                 this.props.onChange(descriptor, !this.props.value)
                 break
             }
 
             case 'STRING': {
                 const {textArea} = this.ref
-                this.props.onChange(descriptor, textArea.value)
+                this.props.onChange(descriptor, textArea.current!.value)
                 break
             }
 
             case 'FLOAT':
             case 'NUMBER': {
                 const input = this.ref.numberInput
-                this.props.onChange(descriptor, input.value)
+                this.props.onChange(descriptor, input.current!.value)
                 break
             }
         }
@@ -252,15 +261,15 @@ export default class DelirValueInput extends React.PureComponent<DelirValueInput
     {
         e.preventDefault()
         e.stopPropagation()
-        this.ref.textInputDropdown.show(() => {
-            setTimeout(() => this.ref.textArea.focus(), 0)
+        this.ref.textInputDropdown.current!.show(() => {
+            setTimeout(() => this.ref.textArea.current!.focus(), 0)
         })
     }
 
     private onKeydownTextArea = (e: React.KeyboardEvent<HTMLTextAreaElement>) =>
     {
         if ((e.metaKey === true || e.ctrlKey === true) && e.key === 'Enter') {
-            this.ref.textInputDropdown.hide()
+            this.ref.textInputDropdown.current!.hide()
             this.valueChanged()
         }
     }
@@ -268,7 +277,7 @@ export default class DelirValueInput extends React.PureComponent<DelirValueInput
     private openColorPicker = (e: React.MouseEvent<HTMLButtonElement>) =>
     {
         const {colorPickerDropdown} = this.ref
-        colorPickerDropdown.show()
+        colorPickerDropdown.current!.show()
 
         e.preventDefault()
         e.stopPropagation()
@@ -277,20 +286,10 @@ export default class DelirValueInput extends React.PureComponent<DelirValueInput
     private closeColorPicker = (e: React.KeyboardEvent<HTMLDivElement>) =>
     {
         const {colorPickerDropdown} = this.ref
-        colorPickerDropdown.hide()
+        colorPickerDropdown.current!.hide()
         this.valueChanged()
 
         e.preventDefault()
         e.stopPropagation()
     }
-
-    private bindColorPickerDropdown = el => { this.ref.colorPickerDropdown = el }
-    private bindColorPicker = el => { this.ref.colorPicker = el }
-    private bindCheckbox = el => { this.ref.checkbox = el }
-    private bindTextInputDropdown = el => { this.ref.textInputDropdown = el }
-    private bindTextArea = el => { this.ref.textArea = el }
-    private bindTextSummary = el => { this.ref.textSummary = el }
-    private bindNumberInput = el => { this.ref.numberInput = el }
-    private bindEnumSelect = el => { this.ref.enumSelect = el }
-    private bindAssetSelect = el => { this.ref.assetSelect = el }
 }
