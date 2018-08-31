7316087b46b3dfb977ab67e435f311cbd8a1ba56
ragg <ragg.devpr@gmail.com>
2018-08-31 16:05:41 +0900

    Rename .babelrc -> babel.config.js
    https://github.com/babel/website/blob/master/docs/config-files.md#6x-vs-7x-babelrc-loading

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "presets": [
+        ["@babel/env", {
+            "modules": false,
+            "targets": {
+                "node": true
+            }
+        }]
+    ],
+    "plugins": [
+        "@babel/plugin-transform-modules-commonjs",
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-object-rest-spread"
+    ]
+}
