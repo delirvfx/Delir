5a141f951fed172bc1f06f83583c5c94a18f39da
ragg <ragg.devpr@gmail.com>
2018-08-31 22:00:35 +0900

    Remove /project

--- packages/delir-core/src/project/scheme/project.ts
+++
@@ -1,8 +0,0 @@
-import { AssetScheme } from './asset'
-import { CompositionScheme } from './composition'
-
-export interface ProjectScheme {
-    formatVersion: string
-    assets: AssetScheme[]
-    compositions: CompositionScheme[]
-}
