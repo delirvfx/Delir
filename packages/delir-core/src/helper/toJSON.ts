4fe883c9468f8eecd079136decf28f369819296d
ragg <ragg.devpr@gmail.com>
2018-09-02 23:08:07 +0900

    Remove unused modules

--- packages/delir-core/src/helper/toJSON.ts
+++
@@ -1,3 +0,0 @@
-export default <T = any>(obj: T): T => {
-    return JSON.parse(JSON.stringify(obj))
-}
--- packages/delir/utils/Flux/Dispatcher.ts
+++
@@ -1,4 +0,0 @@
-import { Dispatcher } from 'flux'
-import { KnownPayload } from '../../actions/PayloadTypes'
-
-export default new Dispatcher<KnownPayload>()
