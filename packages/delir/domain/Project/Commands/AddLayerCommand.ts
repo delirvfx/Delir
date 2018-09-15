21963795486e7a65ece97be89195e22c92770c3d
ragg <ragg.devpr@gmail.com>
2018-09-15 18:24:32 +0900

    Adjust name

--- packages/delir/domain/Project/Commands/AddLayerCommand.ts
+++ packages/delir/domain/Project/Commands/AddLayerCommand.ts
@@ -7,19 +7,19 @@ import { ProjectActions }  from '../actions'
 export default class CreateLayerCommand implements Command {
     constructor(
         private targetCompositionId: string,
-        private createdLayer: Delir.Entity.Layer,
+        private addedLayer: Delir.Entity.Layer,
     ) {}
 
     public undo(context: OperationContext<any>) {
         context.dispatch(ProjectActions.removeLayerAction, {
-            targetLayerId: this.createdLayer.id,
+            targetLayerId: this.addedLayer.id,
         })
     }
 
     public redo(context: OperationContext<any>) {
         context.dispatch(ProjectActions.addLayerAction, {
             targetCompositionId: this.targetCompositionId,
-            layer: this.createdLayer,
+            layer: this.addedLayer,
         })
     }
 }
