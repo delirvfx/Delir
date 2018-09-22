325b701ed811c927d2436d914ffa56fa8fa1c0e3
ragg <ragg.devpr@gmail.com>
2018-09-22 15:44:15 +0900

    Fix type

--- packages/delir-core/src/helper/proxyFreeze.ts
+++ packages/delir-core/src/helper/proxyFreeze.ts
@@ -4,7 +4,7 @@ export const proxyDeepFreeze = <T extends object> (obj: T): DeepReadonly<T> => {
     return obj instanceof Object
         ? new Proxy(obj as any, {
             get(target: T, property: string) {
-                return proxyDeepFreeze(target[property])
+                return proxyDeepFreeze((target as any)[property])
             },
             set() {
                 throw new Error('Can not set value Object is readonly')
