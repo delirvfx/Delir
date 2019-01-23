361e6553f2d35a7f21031e32061620e832f1a469
ragg <ragg.devpr@gmail.com>
2019-01-23 21:53:10 +0900

    Order initialize process

--- packages/delir/main.ts
+++ packages/delir/main.ts
@@ -51,14 +51,14 @@ window.addEventListener('DOMContentLoaded', async () => {
         enableTimeTravel: false,
     }))
 
-    ReactDOM.render(createElementWithContext(context, AppView, {}), document.querySelector('#root'), () => {
-        ;(document.querySelector('#loading') as HTMLElement).style.display = 'none'
-    })
-
     await context.executeOperation(RendererOps.loadPlugins, {})
     await context.executeOperation(PreferenceOps.restoreApplicationPreference, {})
     await context.executeOperation(EditorOps.setActiveProject, {
         project: new Delir.Entity.Project({}),
+    })
+
+    ReactDOM.render(createElementWithContext(context, AppView, {}), document.querySelector('#root'), () => {
+        ;(document.querySelector('#loading') as HTMLElement).style.display = 'none'
     })
 
     if (__DEV__) {
