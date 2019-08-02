685314187c7f2a77f83118cf0475b7b69733115b
Mitsuka Hanakura a.k.a. ragg <ragg.devpr@gmail.com>
2019-08-02 18:13:37 +0900

    Add hooks util

---
+++ packages/delir/src/utils/hooks.ts
@@ -0,0 +1,9 @@
+import { useEffect, useRef } from 'react'
+
+export const usePrevious = <T>(value: T): T | null => {
+  const ref = useRef<T | null>(null)
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+  return ref.current
+}
