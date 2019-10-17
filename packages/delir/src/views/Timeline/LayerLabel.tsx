bfa7909615ba03ed0af231eb52f2436db5301611
Mitsuka Hanakura <ragg.devpr@gmail.com>
2019-10-18 01:06:39 +0900

    Refactor LayerLabel to using hooks

--- packages/delir/src/views/Timeline/LayerLabel.tsx
+++ packages/delir/src/views/Timeline/LayerLabel.tsx
@@ -1,7 +1,7 @@
 import * as Delir from '@delirvfx/core'
-import { connectToStores, ContextProp, withFleurContext } from '@fleur/react'
+import { connectToStores, ContextProp, useFleurContext, useStore, withFleurContext } from '@fleur/react'
 import classnames from 'classnames'
-import React from 'react'
+import React, { useCallback, useRef } from 'react'
 import { SortableElement, SortableHandle } from 'react-sortable-hoc'
 
 import EditorStore from '../../domain/Editor/EditorStore'
@@ -10,6 +10,7 @@ import * as ProjectOps from '../../domain/Project/operations'
 import { ContextMenu, MenuItem, MenuItemOption } from '../../components/ContextMenu/ContextMenu'
 import { LabelInput } from '../../components/LabelInput'
 
+import { getActiveComp } from 'domain/Editor/selectors'
 import t from './LayerLabel.i18n'
 import s from './LayerLabel.sass'
 
@@ -26,91 +27,89 @@ interface OwnProps {
   onRemove: (layerId: string) => any
 }
 
-interface ConnectedProps {
-  activeComp: Delir.Entity.Composition | null
-}
-
-type Props = OwnProps & ConnectedProps & ContextProp
+type Props = OwnProps
 
 const SortHandle = SortableHandle(() => <i className="fa fa-bars" />)
 
-class LayerLabelComponent extends React.Component<Props> {
-  private layerNameInput: LabelInput
+const LayerLabelComponent = ({ layer, layerIndex, onSelect, onRemove }: Props) => {
+  const { executeOperation } = useFleurContext()
+  const { activeComp } = useStore([EditorStore], getStore => ({
+    activeComp: getActiveComp(getStore),
+  }))
 
-  public render() {
-    const { layer, activeComp } = this.props
+  const layerNameInputRef = useRef<LabelInput | null>(null)
 
-    return (
-      <ul key={layer.id} className={s.LaneLabel}>
-        <ContextMenu>
-          <MenuItem type="separator" />
-          <MenuItem label={t(t.k.contextMenu.renameLayer)} onClick={this.focusToLayerNameInput} />
-          <MenuItem label={t(t.k.contextMenu.removeLayer)} data-layer-id={layer.id} onClick={this.onRemove} />
-          <MenuItem type="separator" />
-          <MenuItem label={t(t.k.contextMenu.addLayerHere)} onClick={this.handleAddLayer} />
-        </ContextMenu>
+  const handleChangeLayerName = useCallback(
+    (value: string) => {
+      executeOperation(ProjectOps.modifyLayer, {
+        layerId: layer.id!,
+        patch: { name: value },
+      })
+    },
+    [layer],
+  )
 
-        <li className={classnames(s.LaneLabel_Col, s['LaneLabel_Col--Handle'])}>
-          <SortHandle />
-        </li>
+  const handleFocusLayerNameInput = useCallback(() => {
+    layerNameInputRef.current!.enableAndFocus()
+  }, [])
 
-        <li
-          className={classnames(s.LaneLabel_Col, s['LaneLabel_Col--col-name'])}
-          data-layer-id={layer.id}
-          onClick={this.onSelect}
-        >
-          <LabelInput
-            ref={this.bindLayerNameInput}
-            defaultValue={layer.name}
-            placeholder="Layer name"
-            onChange={this.layerNameChanged}
-          />
-        </li>
-        <li className={classnames(s.LaneLabel_Col, s['LaneLabel_Col--col-visibility'])}>
-          <i className="twa twa-eye" />
-        </li>
-        <li className={classnames(s.LaneLabel_Col, s['LaneLabel_Col--col-lock'])}>
-          <i className="twa twa-lock" />
-        </li>
-      </ul>
-    )
-  }
+  const handleRemoveLayer = useCallback(
+    ({ dataset }: MenuItemOption<{ layerId: string }>) => {
+      onRemove(dataset.layerId)
+    },
+    [onRemove],
+  )
 
-  private layerNameChanged = (value: string) => {
-    const { layer } = this.props
-
-    this.props.executeOperation(ProjectOps.modifyLayer, {
-      layerId: layer.id!,
-      patch: { name: value },
-    })
-  }
-
-  private focusToLayerNameInput = () => {
-    this.layerNameInput.enableAndFocus()
-  }
-
-  private bindLayerNameInput = (el: LabelInput) => (this.layerNameInput = el)
-
-  private onSelect = ({ currentTarget }: React.MouseEvent<HTMLLIElement>) =>
-    this.props.onSelect(currentTarget.dataset.layerId!)
-
-  private onRemove = ({ dataset }: MenuItemOption<{ layerId: string }>) => this.props.onRemove(dataset.layerId)
-
-  private handleAddLayer = () => {
-    const { activeComp, layerIndex } = this.props
+  const handleAddLayer = useCallback(() => {
     if (!activeComp) return
 
-    this.props.executeOperation(ProjectOps.addLayer, {
+    executeOperation(ProjectOps.addLayer, {
       targetCompositionId: activeComp.id,
       index: layerIndex,
     })
-  }
+  }, [activeComp, layerIndex])
+
+  const handleClickLayer = useCallback(
+    ({ currentTarget }: React.MouseEvent<HTMLLIElement>) => {
+      onSelect(currentTarget.dataset.layerId!)
+    },
+    [onSelect],
+  )
+
+  return (
+    <ul key={layer.id} className={s.LaneLabel}>
+      <ContextMenu>
+        <MenuItem type="separator" />
+        <MenuItem label={t(t.k.contextMenu.renameLayer)} onClick={handleFocusLayerNameInput} />
+        <MenuItem label={t(t.k.contextMenu.removeLayer)} data-layer-id={layer.id} onClick={handleRemoveLayer} />
+        <MenuItem type="separator" />
+        <MenuItem label={t(t.k.contextMenu.addLayerHere)} onClick={handleAddLayer} />
+      </ContextMenu>
+
+      <li className={classnames(s.LaneLabel_Col, s['LaneLabel_Col--Handle'])}>
+        <SortHandle />
+      </li>
+
+      <li
+        className={classnames(s.LaneLabel_Col, s['LaneLabel_Col--col-name'])}
+        data-layer-id={layer.id}
+        onClick={handleClickLayer}
+      >
+        <LabelInput
+          ref={layerNameInputRef}
+          defaultValue={layer.name}
+          placeholder="Layer name"
+          onChange={handleChangeLayerName}
+        />
+      </li>
+      <li className={classnames(s.LaneLabel_Col, s['LaneLabel_Col--col-visibility'])}>
+        <i className="twa twa-eye" />
+      </li>
+      <li className={classnames(s.LaneLabel_Col, s['LaneLabel_Col--col-lock'])}>
+        <i className="twa twa-lock" />
+      </li>
+    </ul>
+  )
 }
 
-const LayerLabelConnected = withFleurContext(
-  connectToStores([EditorStore], getStore => ({
-    activeComp: getStore(EditorStore).getActiveComposition(),
-  }))(LayerLabelComponent),
-)
-
-export const LayerLabel = SortableElement((props: OwnProps) => <LayerLabelConnected {...props} />)
+export const LayerLabel = SortableElement((props: OwnProps) => <LayerLabelComponent {...props} />)
