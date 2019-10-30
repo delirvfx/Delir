96c3dfcf082b23bb4c82d7dae824a26b7fff15f0
Mitsuka Hanakura <ragg.devpr@gmail.com>
2019-10-31 00:36:49 +0900

    Improvement Preview controlling
    - Support pinch / two-finger movement in macOS
    - Middle drag to move preview

--- packages/delir/src/views/PreviewView/PreviewView.tsx
+++ packages/delir/src/views/PreviewView/PreviewView.tsx
@@ -1,15 +1,20 @@
 import { StoreGetter } from '@fleur/fleur'
-import { connectToStores, ContextProp, withFleurContext } from '@fleur/react'
-import React from 'react'
+import { ContextProp, useFleurContext, useStore } from '@fleur/react'
+import React, { MouseEvent, useCallback, useRef } from 'react'
 import { frameToTimeCode } from '../../utils/Timecode'
 
 import { Dropdown } from '../../components/Dropdown'
 import { Pane } from '../../components/Pane'
 
+import { getActiveComp, getCurrentPreviewFrame } from 'domain/Editor/selectors'
 import EditorStore from '../../domain/Editor/EditorStore'
 import * as RendererOps from '../../domain/Renderer/operations'
-import RendererStore, { RenderState } from '../../domain/Renderer/RendererStore'
+import RendererStore from '../../domain/Renderer/RendererStore'
 
+import { useEffect } from 'react'
+import { WheelEvent } from 'react'
+import { useObjectState } from 'utils/hooks'
+import { Platform } from 'utils/platform'
 import t from './PreviewView.i18n'
 import s from './PreviewView.sass'
 
@@ -25,123 +30,149 @@ interface State {
 const mapStoresToProps = (getStore: StoreGetter) => {
   const editorStore = getStore(EditorStore)
 
-  return {
-    activeComp: editorStore.getState().activeComp,
-    currentPreviewFrame: editorStore.getState().currentPreviewFrame,
-    previewPlaying: getStore(RendererStore).previewPlaying,
-    lastRenderState: getStore(RendererStore).getLastRenderState(),
-  }
+  return
 }
 
-export default withFleurContext(
-  connectToStores([EditorStore, RendererStore], mapStoresToProps)(
-    class PreviewView extends React.Component<Props, State> {
-      public state = {
-        scale: 1,
-        scaleListShown: false,
-        positionX: 0,
-        positionY: 0,
+export const PreviewView = (props: Props) => {
+  const { executeOperation } = useFleurContext()
+  const { activeComp, currentPreviewFrame, previewPlaying, lastRenderState } = useStore(
+    [EditorStore, RendererStore],
+    getStore => ({
+      activeComp: getActiveComp(getStore),
+      currentPreviewFrame: getCurrentPreviewFrame(getStore),
+      previewPlaying: getStore(RendererStore).previewPlaying,
+      lastRenderState: getStore(RendererStore).getLastRenderState(),
+    }),
+  )
+
+  const [{ scale, positionX, positionY, scaleListShown }, setState] = useObjectState<State>({
+    scale: 1,
+    scaleListShown: false,
+    positionX: 0,
+    positionY: 0,
+  })
+
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const scaleListRef = useRef<Dropdown | null>(null)
+  const mouseMovement = useRef<{ baseScreenX: number; baseScreenY: number } | null>({ baseScreenX: 0, baseScreenY: 0 })
+
+  const handleWheelPreviewView = useCallback(
+    (e: WheelEvent<HTMLDivElement>) => {
+      if (e.altKey && !e.ctrlKey) {
+        setState({
+          scale: Math.max(0.1, Math.min(scale + -e.deltaY / 20, 3)),
+        })
       }
 
-      private scaleListRef = React.createRef<Dropdown>()
-      private canvasRef = React.createRef<HTMLCanvasElement>()
-
-      public componentDidMount() {
-        this.props.executeOperation(RendererOps.setPreviewCanvas, {
-          canvas: this.canvasRef.current!,
-        })
-
-        window.addEventListener('wheel', e => {
-          this.setState(state =>
-            e.ctrlKey
-              ? { scale: Math.max(0.05, state.scale - e.deltaY * 0.01) }
-              : {
-                  positionX: state.positionX - e.deltaX * 2,
-                  positionY: state.positionY - e.deltaY * 2,
-                },
-          )
-        })
-      }
-
-      public render() {
-        const { activeComp, currentPreviewFrame, previewPlaying, lastRenderState } = this.props
-        const { scale, positionX, positionY, scaleListShown } = this.state
-        const currentScale = Math.round(scale * 100)
-        const width = activeComp ? activeComp.width : 640
-        const height = activeComp ? activeComp.height : 360
-        const currentFrame = previewPlaying && lastRenderState ? lastRenderState.currentFrame : currentPreviewFrame
-        const timecode = activeComp ? frameToTimeCode(currentFrame, activeComp.framerate) : '--:--:--:--'
-
-        return (
-          <Pane className={s.Preview} allowFocus>
-            <div className={s.Preview_Inner}>
-              <div className={s.Preview_Header}>{activeComp && activeComp.name}</div>
-              <div className={s.Preview_View} onWheel={this.onWheel}>
-                <canvas
-                  ref={this.canvasRef}
-                  className={s.PreviewView_Canvas}
-                  width={width}
-                  height={height}
-                  style={{
-                    transform: `scale(${this.state.scale}) translateX(${positionX}px)
-                      translateY(${positionY}px)`,
-                  }}
-                />
-              </div>
-              <div className={s.Preview_Footer}>
-                <label className={s.FooterItem} onClick={this.toggleScaleList}>
-                  <i className="fa fa-search-plus" />
-                  <span className={s.currentScale}>{currentScale}%</span>
-                  <Dropdown ref={this.scaleListRef} className={s.dropdown} shownInitial={scaleListShown}>
-                    <div data-value="50" onClick={this.handleSelectScale}>
-                      50%
-                    </div>
-                    <div data-value="100" onClick={this.handleSelectScale}>
-                      100%
-                    </div>
-                    <div data-value="150" onClick={this.handleSelectScale}>
-                      150%
-                    </div>
-                    <div data-value="200" onClick={this.handleSelectScale}>
-                      200%
-                    </div>
-                    <div data-value="250" onClick={this.handleSelectScale}>
-                      250%
-                    </div>
-                    <div data-value="300" onClick={this.handleSelectScale}>
-                      300%
-                    </div>
-                  </Dropdown>
-                </label>
-                <div className={s.FooterItem}>{timecode}</div>
-              </div>
-            </div>
-          </Pane>
+      if (Platform.isMacOS) {
+        setState(
+          e.ctrlKey
+            ? { scale: Math.max(0.05, scale - e.deltaY * 0.01) }
+            : {
+                positionX: positionX - e.deltaX,
+                positionY: positionY - e.deltaY,
+              },
         )
       }
-
-      private handleSelectScale = (e: React.MouseEvent<HTMLDivElement>) => {
-        e.stopPropagation()
-
-        this.scaleListRef.current!.hide()
-
-        this.setState({
-          scale: parseInt(e.currentTarget.dataset.value!, 10) / 100,
-          scaleListShown: false,
-        })
-      }
-
-      private toggleScaleList = () => {
-        this.scaleListRef.current!.toggle()
-      }
-
-      private onWheel = (e: React.WheelEvent<HTMLDivElement>) => {
-        if (!e.altKey) return
-
-        this.setState({
-          scale: Math.max(0.1, Math.min(this.state.scale + -e.deltaY / 20, 3)),
-        })
-      }
     },
-  ),
-)
+    [scale, positionX, positionY],
+  )
+
+  const handleMouseDown = useCallback((e: MouseEvent<HTMLDivElement>) => {
+    if (e.nativeEvent.which !== 2) return // Middle click
+  }, [])
+
+  const handleMouseMove = useCallback(
+    (e: MouseEvent<HTMLDivElement>) => {
+      if (e.nativeEvent.which !== 2) return // Middle click
+
+      setState(state => ({
+        positionX: state.positionX + e.movementX * (1 / scale),
+        positionY: state.positionY + e.movementY * (1 / scale),
+      }))
+    },
+    [scale],
+  )
+
+  const handleMouseUp = useCallback(() => {
+    mouseMovement.current = null
+  }, [])
+
+  const handleToggleScaleList = useCallback(() => {
+    scaleListRef.current!.toggle()
+  }, [])
+
+  const handleSelectScale = useCallback((e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation()
+    scaleListRef.current!.hide()
+
+    setState({
+      scale: parseInt(e.currentTarget.dataset.value!, 10) / 100,
+      positionX: 0,
+      positionY: 0,
+      scaleListShown: false,
+    })
+  }, [])
+
+  useEffect(() => {
+    executeOperation(RendererOps.setPreviewCanvas, { canvas: canvasRef.current! })
+  }, [])
+
+  const displayScale = Math.round(scale * 100)
+  const width = activeComp ? activeComp.width : 640
+  const height = activeComp ? activeComp.height : 360
+  const currentFrame = previewPlaying && lastRenderState ? lastRenderState.currentFrame : currentPreviewFrame
+  const timecode = activeComp ? frameToTimeCode(currentFrame, activeComp.framerate) : '--:--:--:--'
+
+  return (
+    <Pane className={s.Preview} allowFocus>
+      <div className={s.Preview_Inner}>
+        <div className={s.Preview_Header}>{activeComp && activeComp.name}</div>
+        <div
+          className={s.Preview_View}
+          onWheel={handleWheelPreviewView}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        >
+          <canvas
+            ref={canvasRef}
+            className={s.PreviewView_Canvas}
+            width={width}
+            height={height}
+            style={{
+              transform: `scale(${scale}) translateX(${positionX}px) translateY(${positionY}px)`,
+            }}
+          />
+        </div>
+        <div className={s.Preview_Footer}>
+          <label className={s.FooterItem} onClick={handleToggleScaleList}>
+            <i className="fa fa-search-plus" />
+            <span className={s.currentScale}>{displayScale}%</span>
+            <Dropdown ref={scaleListRef} className={s.dropdown} shownInitial={scaleListShown}>
+              <div data-value="50" onClick={handleSelectScale}>
+                50%
+              </div>
+              <div data-value="100" onClick={handleSelectScale}>
+                100%
+              </div>
+              <div data-value="150" onClick={handleSelectScale}>
+                150%
+              </div>
+              <div data-value="200" onClick={handleSelectScale}>
+                200%
+              </div>
+              <div data-value="250" onClick={handleSelectScale}>
+                250%
+              </div>
+              <div data-value="300" onClick={handleSelectScale}>
+                300%
+              </div>
+            </Dropdown>
+          </label>
+          <div className={s.FooterItem}>{timecode}</div>
+        </div>
+      </div>
+    </Pane>
+  )
+}
