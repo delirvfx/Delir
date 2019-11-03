15832e9a20e4177f51d1382c4165c50f5aba1c57
Mitsuka Hanakura <ragg.devpr@gmail.com>
2019-11-03 17:52:38 +0900

    Support multiple clip deletion

--- packages/delir/src/views/Timeline/Clip.tsx
+++ packages/delir/src/views/Timeline/Clip.tsx
@@ -14,6 +14,7 @@ import { MountTransition } from '../../components/MountTransition'
 import * as EditorOps from '../../domain/Editor/operations'
 import * as ProjectOps from '../../domain/Project/operations'
 
+import { getSelectedClipIds } from 'domain/Editor/selectors'
 import t from './Clip.i18n'
 import s from './Clip.sass'
 import { ClipDragProps, withClipDragContext } from './ClipDragContext'
@@ -227,9 +228,15 @@ export default decorate<OwnProps>(
     }
 
     private handleRemoveClip = ({ dataset }: MenuItemOption<{ clipId: string }>) => {
-      this.props.executeOperation(ProjectOps.removeClips, {
-        clipIds: [dataset.clipId],
-      })
+      if (this.props.active) {
+        this.props.executeOperation(ProjectOps.removeClips, {
+          clipIds: getSelectedClipIds(this.props.getStore),
+        })
+      } else {
+        this.props.executeOperation(ProjectOps.removeClips, {
+          clipIds: [dataset.clipId],
+        })
+      }
     }
 
     private handleSeekToHeadOfClip = () => {
