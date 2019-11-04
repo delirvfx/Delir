617d37836e9b464c7b2797ae59a200f57edcb7b8
Mitsuka Hanakura <ragg.devpr@gmail.com>
2019-11-05 00:19:43 +0900

    Strict proxyDeepFreeze behaviour and typing

--- packages/core/src/helper/proxyFreeze.ts
+++ packages/core/src/helper/proxyFreeze.ts
@@ -1,14 +1,24 @@
-type DeepReadonly<T> = { readonly [K in keyof T]: DeepReadonly<T[K]> }
+// prettier-ignore
+export type DeepReadonly<T> =
+  T extends object ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+  : T extends Array<infer R> ? ReadonlyArray<R>
+  : T
 
-export const proxyDeepFreeze = <T extends object>(obj: T): DeepReadonly<T> => {
-  return obj instanceof Object
-    ? new Proxy(obj as any, {
-        get(target: T, property: string) {
-          return proxyDeepFreeze((target as any)[property])
-        },
-        set() {
-          throw new Error('Can not set value Object is readonly')
-        },
-      })
-    : obj
+export const proxyDeepFreeze = <T extends any>(obj: T): DeepReadonly<T> => {
+  if (obj === null) {
+    return obj as DeepReadonly<T>
+  }
+
+  if (typeof obj === 'object') {
+    return new Proxy<T>(obj, {
+      get(target: T, property: string) {
+        return proxyDeepFreeze((target as any)[property])
+      },
+      set() {
+        throw new Error('Can not set value Object is readonly')
+      },
+    })
+  }
+
+  return obj as DeepReadonly<T>
 }
