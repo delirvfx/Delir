160f8571d86d38f02a8dac803cf4487fb70e350f
Hanakla <ragg.devpr@gmail.com>
2019-11-10 16:06:41 +0900

    Add repeat-tile effect

--- packages/post-effect-plugins/repeat-tile/index.ts
+++ packages/post-effect-plugins/repeat-tile/index.ts
@@ -3,7 +3,26 @@ import { EffectPreRenderContext, EffectRenderContext, ParamType, PostEffectBase,
 interface Params {
   xRepeat: ParamType.Number
   yRepeat: ParamType.Number
+  xOffset: ParamType.Float
+  yOffset: ParamType.Float
 }
+
+const FRAGMENT_SHADER = `
+precision mediump float;
+
+uniform sampler2D source;
+uniform vec2 resolution;
+uniform vec2 repeats;
+uniform vec2 offsets;
+varying vec2 vTexCoord;
+
+void main(void) {
+  vec2 offset = vec2(1.0 / resolution.x * offsets.x, 1.0 / resolution.y * offsets.y);
+  vec2 pos = vec2(vTexCoord.x * repeats.x + offset.x, vTexCoord.y * repeats.y + offset.y);
+  vec4 color = texture2D(source, pos);
+  gl_FragColor = color;
+}
+`
 
 export default class Chromakey extends PostEffectBase {
   /**
@@ -12,32 +31,30 @@ export default class Chromakey extends PostEffectBase {
   public static provideParameters() {
     // prettier-ignore
     return Type
-      .number('repeat', { label: 'repeat', defaultValue: () => 0, animatable: true })
+      .number('xRepeat', { label: 'X repeat', defaultValue: () => 2, animatable: true })
+      .number('yRepeat', { label: 'Y repeat', defaultValue: () => 2, animatable: true })
+      .float('xOffset', { label: 'Offset X(px)', defaultValue: () => 0, animatable: true})
+      .float('yOffset', { label: 'Offset Y(px)', defaultValue: () => 0, animatable: true})
   }
 
-  private static FRAGMENT_SHADER: string = require('./fragment.frag').default
   private program: WebGLProgram
 
-  /**
-   * Called when before rendering start.
-   *
-   * If you want initializing before rendering (likes load audio, image, etc...)
-   * Do it in this method.
-   */
-  public async initialize(req: EffectPreRenderContext<Params>) {
-    this.program = req.gl.getProgram(Chromakey.FRAGMENT_SHADER)
+  public async initialize(context: EffectPreRenderContext<Params>) {
+    this.program = context.gl.getProgram(FRAGMENT_SHADER)
   }
 
-  /**
-   * Render frame into destination canvas.
-   * @param req
-   */
-  public async render(req: EffectRenderContext<Params>) {
-    const {
-      gl,
+  public async render(context: EffectRenderContext<Params>) {
+    const { gl, srcCanvas, destCanvas, parameters: params } = context
+
+    gl.applyProgram(
+      this.program,
+      {
+        resolution: gl.uni2fv([context.width, context.height]),
+        repeats: gl.uni2fv([params.xRepeat, params.yRepeat]),
+        offsets: gl.uni2fv([params.xOffset, params.yOffset]),
+      },
       srcCanvas,
       destCanvas,
-      parameters: { xRepeat, yRepeat },
-    } = req
+    )
   }
 }
