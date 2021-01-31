60eb244de65ff2457b3976e421d1de5e32b1c80c
Hanakla / ragg <ragg@pixiv.com>
2021-01-31 18:18:36 +0900

    Fix import

--- packages/core/src/SpecHelper.ts
+++ packages/core/src/SpecHelper.ts
@@ -1,4 +1,4 @@
-import { Entity, Values } from 'index'
+import { Entity, Values } from './index'
 
 const safeAssign = <T extends object>(dest: T, ...source: Partial<T>[]): T => {
   return Object.assign(dest, ...source)
